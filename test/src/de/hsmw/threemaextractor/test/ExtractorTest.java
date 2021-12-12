67e796f7743fd2eaeb7d3b7abb24be69adf858de
Erik Tschierschke <56506427+ErikTschierschke@users.noreply.github.com>
2021-12-12 12:59:30 +0100

    add test plugin

---
+++ test/src/de/hsmw/threemaextractor/test/ExtractorTest.java
@@ -0,0 +1,36 @@
+package de.hsmw.threemaextractor.test;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+import de.hsmw.threemaextractor.service.main.ChatVisualizer;
+import de.hsmw.threemaextractor.service.main.FileStore;
+import de.hsmw.threemaextractor.service.main.MainDatabase;
+import de.hsmw.threemaextractor.service.main.ThreemaExtractor;
+
+
+public class ExtractorTest implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		
+
+		FileStore fileStore = new FileStore(
+				"res/key.dat", "res/threema4.db", "res/ch.threema.app_preferences.xml", "res/data/", "out/");
+		
+		ThreemaExtractor threemaExtractor = new ThreemaExtractor(fileStore);
+		
+		MainDatabase mainDatabase = threemaExtractor.getMainDatabase();
+		
+		ChatVisualizer chatVisualizer = new  ChatVisualizer(mainDatabase.getContacts(), true, true);
+		
+		System.out.println(chatVisualizer.visualizeDirectConversation(mainDatabase.getDirectMessages(), "XNYZKFYJ"));
+		
+		return IApplication.EXIT_OK;
+	}
+
+	@Override
+	public void stop() {
+		// nothing to do
+	}
+}
